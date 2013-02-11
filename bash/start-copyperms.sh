cbe8481669c3eb9e834da67160ce0c5532e9b3c0
Kristof Kowalski <kristof@kowalski.ms>
2013-02-11 17:11:55 +1100

    Add start-copyperms.sh
    This script will add www-data user to user group. It will copy user
    permissions over to the group. Running Wordpress under www-data is not a
    great idea.

---
+++ bash/start-copyperms.sh
@@ -0,0 +1,27 @@
+#
+# [Ubuntu] Sort out Apache www-data perms
+#
+# This script will cope the user permission to the group permission
+#
+
+# Set owner:group defaults
+OWNER="username"
+GROUP="groupname"
+
+# Add www-data user to group
+usermod -G ${GROUP} www-data 
+
+# Set directory to change
+DIR="/dir1/dir2/"
+
+FILES=$(find ${DIR}.)
+for F in $FILES
+do		
+	# Set owner/group 
+	chown ${OWNER}:${GROUP} ${F}
+	# Get file access rights
+	PERMS=$(stat -c%a ${F})	
+	# Apple owner => group file access rights
+	chmod ${PERMS:0:1}${PERMS:0:1}${PERMS:2:1} ${F}
+	echo "${PERMS:0:1}${PERMS:1:1}${PERMS:2:1} => ${PERMS:0:1}${PERMS:0:1}${PERMS:2:1} - ${F}"	
+done
